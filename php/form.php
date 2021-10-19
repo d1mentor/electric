fa19dce3623026373fa8da6a5e20fecdde1cf2fb
d1mentor <gerasimenkot92@gmail.com>
2021-10-19 13:02:39 +0300

    fix send form

--- php/form.php
+++ php/form.php
@@ -1,9 +1,9 @@
 <?php
 
-$name = filter_input(INPUT_POST, 'name');
-$phone = filter_input(INPUT_POST, 'phone');
-$email = filter_input(INPUT_POST, 'email');
-$subj = filter_input(INPUT_POST, 'subject');
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+$subj = $_POST["subject"];
 
 $name = htmlspecialchars($name);
 $phone = htmlspecialchars($phone);
@@ -20,10 +20,29 @@ $phone = trim($phone);
 $email = trim($email);
 $subj = trim($subj);
 
-mail("gerasim1305@gmail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email." Phone:".$phone." Subject: ".$subj ,"From: example@gmail.ru \r\n");
+$sites = array(
+    "en" => "en",
+    "de" => "de",
+	"pl" => "pl",
+	"ru" => "ru",
+	"ua" => "ua",
+
+);
+
+// получаем язык
+$loc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+// проверяем язык
+if (!in_array($loc, array_keys($sites))){
+    $loc = 'en';
+}
+
+mail("gerasimenko1305@gmail.com", "Заявка с сайта", "ФИО:".$name.".\n E-mail: ".$email." \nPhone:".$phone." \nSubject: ".$subj."\nFrom: electric, Location:".$sites[$loc] ,"From: ".$email." \r\n");
+
 
 $redirect = isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER']:'redirect-form.html';
 header("Location: $redirect");
 exit();
+
 
 ?>
